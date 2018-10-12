5ce07383294422d7cd4b3adb2ff3891f73f9b1c0
Troelbr <troelsbrafn@gmail.com>
2018-10-12 11:51:34 +0200

    alle billeder loaded

--- main.cpp
+++ main.cpp
@@ -6,9 +6,41 @@
   Version: $$version$$
 */
 
+#include<opencv2/core/core.hpp>
 #include <opencv2/highgui.hpp>
+#include "opencv2/opencv.hpp"
+
 
 #include <iostream>
+
+using namespace cv;
+
+cv::Mat calc_histogram(const cv::Mat& input)
+{
+    Mat histo;
+
+    calcHist( std::vector<Mat>{input},{0}, noArray(), histo, {256}, {0, 256});
+
+    return histo;
+}
+
+cv::Mat draw_histogram(const cv::Mat& histogram)
+{
+    int rows = histogram.rows;
+    Mat histImage( rows, rows, CV_8UC1, Scalar(0));
+
+    // find max value of our histogram for normalization
+    double max;
+    minMaxLoc(histogram, nullptr, &max); // only when there is 1 channel (greyscale)
+
+
+    for (int i = 1; i < rows; i++)
+    {
+        double h = rows * (histogram.at<float>(i)/max); //normalize
+        line(histImage,Point(i, rows), Point(i, rows-h),Scalar::all(255));
+    }
+    return histImage;
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,16 +59,45 @@ int main(int argc, char* argv[])
 
     // Load image file
     std::string filepath = parser.get<std::string>("@image");
-    cv::Mat img = cv::imread(filepath);
+    cv::Mat img1 = cv::imread("../ImagesForStudents/Image1.png");
 
     // Check that the image file was actually loaded
-    if (img.empty()) {
+    if (img1.empty()) {
         std::cout << "Input image not found at '" << filepath << "'\n";
         return 1;
     }
 
-    // Show the image
-    cv::imshow("Image", img);
+    // Image 1
+    cv::namedWindow("Image 1", CV_WINDOW_NORMAL);
+    cv::resizeWindow("Image 1", 800, 1000);
+    cv::imshow("Image 1", img1);
+
+    // Image 2
+    cv::Mat img2 = cv::imread("../ImagesForStudents/Image2.png");
+    cv::namedWindow("Image 2", CV_WINDOW_NORMAL);
+    cv::resizeWindow("Image 2", 800, 1000);
+    cv::imshow("Image 2", img2);
+
+    Mat histogram_image2 = calc_histogram(img2);
+    imshow("histogram of Image 2", draw_histogram(histogram_image2));
+
+    // Image 3
+    cv::Mat img3 = cv::imread("../ImagesForStudents/Image3.png");
+    cv::namedWindow("Image 3", CV_WINDOW_NORMAL);
+    cv::resizeWindow("Image 3", 800, 1000);
+    cv::imshow("Image 3", img3);
+
+    // Image 4
+    cv::Mat img4 = cv::imread("../ImagesForStudents/Image4_1.png");
+    cv::namedWindow("Image 4", CV_WINDOW_NORMAL);
+    cv::resizeWindow("Image 4", 800, 1000);
+    cv::imshow("Image 4", img4);
+    // Image 5
+    cv::Mat img5 = cv::imread("../ImagesForStudents/Image5_optional.png");
+    cv::namedWindow("Image 5", CV_WINDOW_NORMAL);
+    cv::resizeWindow("Image 5", 800, 1000);
+    cv::imshow("Image 5", img5);
+
 
     // Wait for escape key press before returning
     while (cv::waitKey() != 27)
